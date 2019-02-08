5a59d4ca94942079ffd87a81a7c250287e1757a5
shikha jhamb <jhambshikha@yahoo.in>
2019-02-09 00:46:17 +0530

    added checkbox functionality

--- src/welcome/Welcome.js
+++ src/welcome/Welcome.js
@@ -2,6 +2,68 @@ import React, { Component } from "react";
 import "./Welcome.css";
 
 class Welcome extends Component {
+  state = {
+    randomNo1: 0,
+    randomNo2: 0,
+    totalRandom: 0,
+    targetValue: 0
+  };
+
+  randomize = () => {
+    var target = [1, 2, 3, 12, 23, 13];
+    var rand = target[Math.floor(Math.random() * target.length)];
+    this.totalRandom = rand;
+    this.randomNo1 = parseInt(this.totalRandom % 10);
+    this.totalRandom / 10 >= 1
+      ? (this.randomNo2 = parseInt(this.totalRandom / 10))
+      : (this.randomNo2 = 0);
+    this.setState({ totalRandom: this.state.totalRandom });
+    this.setState({ randomNo1: this.state.randomNo1 });
+    this.setState({ randomNo2: this.state.randomNo2 });
+  };
+
+  isChecked = checkId => {
+    if (checkId === this.randomNo1 || checkId === this.randomNo2) return true;
+    else return false;
+  };
+
+  setTargetValue = () => {
+    var target = [1, 2, 3, 12, 23, 13];
+    var rand = target[Math.floor(Math.random() * target.length)];
+    this.targetValue = rand;
+    var targetString = this.targetValue.toString();
+    targetString =
+      targetString.length !== 1
+        ? (targetString =
+            targetString.slice(0, 1) + "," + targetString.slice(1, 2))
+        : targetString;
+
+    return targetString;
+  };
+
+  getStateValue = () => {
+    var result;
+    if (this.randomNo1 !== 0 && this.randomNo2 !== 0) {
+      result =
+        this.randomNo1 === this.randomNo2
+          ? this.randomNo1
+          : this.randomNo1 < this.randomNo2
+          ? this.randomNo1 + "," + this.randomNo2
+          : this.randomNo2 + "," + this.randomNo1;
+    } else {
+      result = this.randomNo1 === 0 ? this.randomNo2 : this.randomNo1;
+    }
+
+    return result;
+  };
+
+  getResult = () => {
+    console.log("total: " + this.totalRandom);
+    console.log("target: " + this.targetValue);
+    var result =
+      this.totalRandom === this.targetValue ? "Correct State" : "Wrong State";
+    return result;
+  };
   render() {
     return (
       <div class="containe">
@@ -16,51 +78,42 @@ class Welcome extends Component {
             "correct state" will be shown
           </p>
         </div>
-        <div class="checkbox">
-          <table>
-            <tr>
-              <td>
-                <h3>Checkboxes</h3>
-              </td>
-              <td>
-                <h3>Local state</h3>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" name="name-1" />
-                checkbox1
-              </td>
-              <td>
-                <input type="text" name="status-1" value="unchecked" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" name="name-2" /> checkbox2
-              </td>
-              <td>
-                <input type="text" name="status-2" value="unchecked" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <input type="checkbox" name="name-3" /> checkbox3
-              </td>
-              <td>
-                <input type="text" name="status-3" value="unchecked" />
-              </td>
-            </tr>
-          </table>
-          target state <input type="text" name="target" value="0" />
+        <div className="checkbox">
+          <div className="header-sub">
+            <h3>Checkboxes</h3>
+            <h3>Local state</h3>
+          </div>
+          <div className="check--container">
+            <input type="checkbox" name="name-1" checked={this.isChecked(1)} />{" "}
+            checkbox1
+            <input type="text" name="status-1" value="unchecked" />
+          </div>
+          <div className="check--container">
+            <input type="checkbox" name="name-2" checked={this.isChecked(2)} />{" "}
+            checkbox2
+            <input type="text" name="status-2" value="unchecked" />
+          </div>
+          <div className="check--container">
+            <input type="checkbox" name="name-3" checked={this.isChecked(3)} />{" "}
+            checkbox3
+            <input type="text" name="status-3" value="unchecked" />
+          </div>
+          <div class="state">
+            Target state{" "}
+            <input type="text" name="target" value={this.setTargetValue()} />
+            Current state{" "}
+            <input type="text" name="current" value={this.getStateValue()} />
+          </div>
           <br />
-          current state <input type="text" name="current" value="0" />
+          <input
+            className="state--button "
+            type="text"
+            name="state"
+            value={this.getResult()}
+          />
           <br />
           <br />
-          <button>check_state</button>
-          <br />
-          <br />
-          <button>randomize</button>
+          <button onClick={this.randomize}>randomize</button>
           <button>submit</button>
         </div>
       </div>
