eab854d00c280601a43a2aab4683cb6528a17e6d
shikha jhamb <jhambshikha@yahoo.in>
2019-02-11 01:49:29 +0530

    edits to index.js file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-//import Navigation from "./navigation/Navigation";
 import Welcome from "./welcome/Welcome";
 import Navigation from "./navigation/Navigation";
+import { BrowserRouter, Route } from "react-router-dom";
+import Checkbox from "./components/selectors/Checkbox";
 
 class App1 extends Component {
   render() {
     return (
       <div>
-        <nav>
-          <Navigation logoTitle="theWeb" />
-        </nav>
-        <aside>
-          <Welcome />
-        </aside>
+        <BrowserRouter>
+          <div>
+            <nav>
+              <Navigation logoTitle="theWeb" />
+            </nav>
+            <Route path="/Checkbox" component={Checkbox} />
+            <Route path="/home" component={Welcome} />
+            <aside>
+              <Welcome />
+            </aside>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
