26d1561cd77a6f10f311b4bd674d9289322eb92f
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-03-02 22:53:43 +0530

    added checkbox select functionality

--- src/components/selectors/Checkbox.js
+++ src/components/selectors/Checkbox.js
@@ -5,73 +5,177 @@ class Checkbox extends Component {
   state = {
     randomNo1: 0,
     randomNo2: 0,
+    randomNo3: 0,
     totalRandom: 0,
-    counter: 0
+    totalCurrent: 0,
+    counter: 0,
+    click: 0,
+    submit: false,
+    checkboxState: [false, false, false]
   };
 
   randomize = () => {
-    var target = [1, 2, 3, 12, 23, 13];
+    console.log("INSIDE RANDOMIZED");
+    this.reset();
+    var target = [1, 2, 3, 12, 23, 13, 123];
     var rand = target[Math.floor(Math.random() * target.length)];
-    this.totalRandom = rand;
-    this.randomNo1 = parseInt(this.totalRandom % 10);
-    this.totalRandom / 10 >= 1
-      ? (this.randomNo2 = parseInt(this.totalRandom / 10))
-      : (this.randomNo2 = 0);
+    this.state.totalRandom = rand;
+    if (rand.toString().length === 3) {
+      this.state.randomNo3 = rand % 10;
+      this.state.totalRandom = parseInt(rand / 10);
+    } else {
+      this.state.randomNo3 = 0;
+    }
+    this.state.randomNo1 = parseInt(this.state.totalRandom % 10);
+    this.state.totalRandom / 10 >= 1
+      ? (this.state.randomNo2 = parseInt(this.state.totalRandom / 10))
+      : (this.state.randomNo2 = 0);
     this.state.counter = 1;
+    this.state.totalRandom = rand;
     this.setState({ totalRandom: this.state.totalRandom });
     this.setState({ randomNo1: this.state.randomNo1 });
     this.setState({ randomNo2: this.state.randomNo2 });
+    this.setState({ randomNo3: this.state.randomNo3 });
+    if (this.state.randomNo1 !== 0) {
+      let checkbox1 = document.getElementById(this.state.randomNo1.toString());
+      checkbox1.checked = true;
+      this.state.checkboxState[this.state.randomNo1 - 1] = true;
+    }
+    if (this.state.randomNo2 !== 0) {
+      let checkbox2 = document.getElementById(this.state.randomNo2.toString());
+      checkbox2.checked = true;
+      this.state.checkboxState[this.state.randomNo2 - 1] = true;
+    }
+    if (this.state.randomNo3 !== 0) {
+      let checkbox3 = document.getElementById(this.state.randomNo3.toString());
+      checkbox3.checked = true;
+      this.state.checkboxState[this.state.randomNo3 - 1] = true;
+    }
+
+    console.log("random 1 => " + this.state.randomNo1);
+    console.log("random 2 => " + this.state.randomNo2);
+    console.log("random 3 => " + this.state.randomNo3);
   };
 
-  isChecked = checkId => {
-    if (checkId === this.randomNo1 || checkId === this.randomNo2) return true;
-    else return false;
+  reset = () => {
+    let highlightedItem = document.querySelectorAll(".check");
+    highlightedItem.forEach(checkbox => {
+      checkbox.checked = false;
+      this.state.checkboxState[parseInt(checkbox.id) - 1] = false;
+    });
+    this.state.submit = false;
+  };
+
+  handleChecked = event => {
+    let id = parseInt(event.target.id);
+    if (this.state.checkboxState[id - 1] === false) {
+      event.target.checked = true;
+      this.state.checkboxState[id - 1] = true;
+      document.getElementById("status-" + id).value = "checked";
+    } else {
+      event.target.checked = false;
+      this.state.checkboxState[id - 1] = false;
+      document.getElementById("status-" + id).value = "unchecked";
+    }
+  };
+
+  submit = () => {
+    this.state.totalCurrent = 0;
+    let currentVal = this.state.totalCurrent;
+    this.state.submit = true;
+    this.state.checkboxState.forEach((current, index) => {
+      if (current === true) {
+        currentVal = currentVal * 10 + index + 1;
+      }
+    });
+    this.state.totalCurrent = currentVal;
+    this.setState({ submit: this.state.submit });
+    this.setState({ totalCurrent: this.state.totalCurrent });
+    this.state.counter = 1;
   };
 
   displayStatus = checkId => {
-    if (checkId === this.randomNo1 || checkId === this.randomNo2)
-      return "checked";
-    else return "unchecked";
+    if (this.state.submit === false) {
+      if (checkId === this.state.randomNo1 || checkId === this.state.randomNo2)
+        return "checked";
+      else return "unchecked";
+    }
   };
 
   getTargetValue = () => {
     if (this.state.counter === 0) {
       return "";
     }
-    var target = [1, 2, 3, 12, 23, 13];
+    var target = [1, 2, 3, 12, 23, 13, 123];
     var rand = target[Math.floor(Math.random() * target.length)];
-    this.targetValue = rand;
-    var targetString = this.targetValue.toString();
+    this.state.targetValue = rand;
+    var targetString = this.state.targetValue.toString();
     targetString =
       targetString.length !== 1
         ? (targetString =
             targetString.slice(0, 1) + "," + targetString.slice(1, 2))
         : targetString;
-
+    if (rand.toString().length === 3) {
+      targetString += "," + (rand % 10);
+    }
     return targetString;
   };
 
-  getStateValue = () => {
-    var result;
-    if (this.randomNo1 !== 0 && this.randomNo2 !== 0) {
-      result =
-        this.randomNo1 === this.randomNo2
-          ? this.randomNo1
-          : this.randomNo1 < this.randomNo2
-          ? this.randomNo1 + "," + this.randomNo2
-          : this.randomNo2 + "," + this.randomNo1;
+  getCurrentValue = () => {
+    let arr = [];
+    let result = "";
+    if (this.state.submit) {
+      this.state.checkboxState.forEach((current, index) => {
+        if (current === true) {
+          arr.push(index + 1);
+        }
+      });
     } else {
-      result = this.randomNo1 === 0 ? this.randomNo2 : this.randomNo1;
+      arr.push(this.state.randomNo1);
+      arr.push(this.state.randomNo2);
+      arr.push(this.state.randomNo3);
     }
-
+    arr.sort();
+    arr.forEach((current, index) => {
+      if (current !== 0) {
+        if (index !== arr.length - 1) {
+          result += current + ",";
+        } else {
+          result += current;
+        }
+      }
+    });
     return result;
   };
 
   getResult = () => {
-    var result =
-      this.totalRandom === this.targetValue ? "Correct State" : "Wrong State";
-    return result;
+    if (this.state.submit === true) {
+      var result =
+        this.state.totalCurrent === this.state.targetValue
+          ? "Correct State"
+          : "Wrong State";
+      return result;
+    } else {
+      var result =
+        this.state.totalRandom === this.state.targetValue
+          ? "Correct State"
+          : "Wrong State";
+      return result;
+    }
   };
+  clear = () => {
+    this.reset();
+    this.state.click = 0;
+    this.state.randomNo1 = 0;
+    this.state.randomNo2 = 0;
+    this.state.randomNo3 = 0;
+    document.querySelector(".text1").value = "";
+    document.querySelector(".text2").value = "";
+    document.getElementById("status-1").value = "unchecked";
+    document.getElementById("status-2").value = "unchecked";
+    document.getElementById("status-3").value = "unchecked";
+  };
+
   render() {
     return (
       <div class="container-checkbox">
@@ -92,25 +196,53 @@ class Checkbox extends Component {
             <h3>Local state</h3>
           </div>
           <div className="checkboxes-row">
-            <input type="checkbox" name="name-1" checked={this.isChecked(1)} />{" "}
+            <input
+              id="1"
+              class="check"
+              type="checkbox"
+              name="name-1"
+              onClick={this.handleChecked}
+            />{" "}
             Checkbox-1
-            <input type="text" name="status-1" value={this.displayStatus(1)} />
+            <input id="status-1" type="text" value={this.displayStatus(1)} />
           </div>
           <div className="checkboxes-row">
-            <input type="checkbox" name="name-2" checked={this.isChecked(2)} />{" "}
+            <input
+              id="2"
+              class="check"
+              type="checkbox"
+              name="name-2"
+              onClick={this.handleChecked}
+            />{" "}
             Checkbox-2
-            <input type="text" name="status-2" value={this.displayStatus(2)} />
+            <input id="status-2" type="text" value={this.displayStatus(2)} />
           </div>
           <div className="checkboxes-row">
-            <input type="checkbox" name="name-3" checked={this.isChecked(3)} />{" "}
+            <input
+              id="3"
+              class="check"
+              type="checkbox"
+              name="name-3"
+              onClick={this.handleChecked}
+            />{" "}
             Checkbox-3
-            <input type="text" name="status-3" value={this.displayStatus(3)} />
+            <input id="status-3" type="text" value={this.displayStatus(3)} />
           </div>
           <div class="textbox-state">
             <b>Target state</b>{" "}
-            <input type="text" name="target" value={this.getTargetValue()} />
+            <input
+              type="text"
+              name="target"
+              class="text1"
+              value={this.getTargetValue()}
+            />
             <b>Current state</b>{" "}
-            <input type="text" name="current" value={this.getStateValue()} />
+            <input
+              type="text"
+              name="current"
+              class="text2"
+              value={this.getCurrentValue()}
+            />
           </div>
           <br />
           <div class="button-primary">
@@ -124,7 +256,8 @@ class Checkbox extends Component {
             <br />
             <div class="button-primary-random">
               <button onClick={this.randomize}>randomize</button>
-              <button>submit</button>
+              <button onClick={this.submit}>submit</button>
+              <button onClick={this.clear}>clear</button>
             </div>
           </div>
         </div>
